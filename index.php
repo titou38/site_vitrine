edbcc3d79d6218837bd5dc43c6d5f9cd1ad26a31
titou38 <anthony.donat@outlook.fr>
2016-03-26 23:36:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,7 @@
 <?php
 included 'top.php';
 ?>
-        <h1>Portfolioe</h1>
-        <nav>
-            <ul>
-                <a href="">
-                    <li>Accueil</li>
-                </a>
-                <a href="">
-                    <li>Ma bio</li>
-                </a>
-                <a href="">
-                    <li>Documents à télécharger</li>
-                </a>
-                <a href="">
-                    <li>Accés au Blog</li>
-                </a>
-                <a href="">
-                    <li>Contact</li>
-            </ul>
-        </nav>
+       <h2>Accueil</h2>
 <?php
 included 'bottom.php';
 ?>
