c436cef0249830c30b579c090b698cedb497e67c
titou38 <anthony.donat@outlook.fr>
2016-03-26 23:35:04 +0100

    Update index.php
    J'ai fait un top fixe et un bottom fixe aussi avec un corps variable d'où le include

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        
-    </head>
-    <body>
-        <h1>Test commit vitrine</h1>
-    </body>
-</html>
+<?php
+included 'top.php';
+?>
+        <h1>Portfolioe</h1>
+        <nav>
+            <ul>
+                <a href="">
+                    <li>Accueil</li>
+                </a>
+                <a href="">
+                    <li>Ma bio</li>
+                </a>
+                <a href="">
+                    <li>Documents à télécharger</li>
+                </a>
+                <a href="">
+                    <li>Accés au Blog</li>
+                </a>
+                <a href="">
+                    <li>Contact</li>
+            </ul>
+        </nav>
+<?php
+included 'bottom.php';
+?>
