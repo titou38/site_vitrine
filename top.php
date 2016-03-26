8cd47935e77d6b8b982e9ebff7a78e4567112ee2
titou38 <anthony.donat@outlook.fr>
2016-03-26 23:36:35 +0100

    Update top.php

--- top.php
+++ top.php
@@ -4,3 +4,22 @@
     <link rel="stylesheet" type="text/css" href="portfolio.css"/>
   </head>
   <body>
+         <h1>Portfolioe</h1>
+        <nav>
+            <ul>
+                <a href="">
+                    <li>Accueil</li>
+                </a>
+                <a href="">
+                    <li>Ma bio</li>
+                </a>
+                <a href="">
+                    <li>Documents à télécharger</li>
+                </a>
+                <a href="">
+                    <li>Accés au Blog</li>
+                </a>
+                <a href="">
+                    <li>Contact</li>
+            </ul>
+        </nav>
