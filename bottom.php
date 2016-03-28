ed6b4d882356573cfa063e0a1a117a1fe71753d1
titou38 <anthony.donat@outlook.fr>
2016-03-28 22:29:48 +0200

    Update bottom.php

--- bottom.php
+++ bottom.php
@@ -1,4 +1,27 @@
 </body>
+<footer>
+  <div class="footer">
+    <ul>
+      <li>Contact</li>
+        <ul>
+          <li>//Mail</li>
+          <li>//PhoneNumber</li>
+          <li>//SocialNetwork</li>
+        </ul>
+      <li>Adresse</li>
+      <p>
+        6 Rue de la République </br> Appartement droite </br> 65962 Lille Cedex9 </br> France
+      </p>
+        <form methode="post"></form>
+          <fieldset>Commentaire</fieldset>
+          <input type="text" value="Nom et prénom"></input>
+          <input type="text" value="Objet du commentaire"></input>
+          <input type="text" value="(Commentaire vide)"></input>
+          <input type="submit" value="Envoyer le message"></input>
+        </form>
+    </ul>
+  </div>
+</footer>
 <script type="text/javascript" src="http://code.jquery.com/jquery-2.2.0.min.js"></script>
 <script type="text/javascript" src="js/javascript.js"></script>
 </html>
