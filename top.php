ce46a4664ffe1938759a9b70bf1ff00892d49cb7
titou38 <anthony.donat@outlook.fr>
2016-03-28 21:56:23 +0200

    Update top.php
    Ajout de meta de referencement si info http://www.alsacreations.com/article/lire/628-balises-meta.html#incontournables

--- top.php
+++ top.php
@@ -2,6 +2,12 @@
   <head>
     <title>Portfolio</title>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <meta name="keywords" content="portfolio, exemple, sebastien, anthony, donat, jourdan, portefeuille, compétences">
+    <meta name="description" content="Ceci est un site d'exemple de portfolio pour des techniciens en développement web, qui sert de base.">
+    <meta name="geo.placename" content="Grenoble, Isère, France">
+    <meta name="twitter:site" content="@anthonydonat">
+    <meta name="theme-color" content="#046380">
     <link rel="stylesheet" type="text/css" href="css/portfolio.css"/>
   </head>
   <body>
